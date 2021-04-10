507f365d85b28f6a3f2733fe23899d58806d64ea
zhaomangang <zhaomangang@qq.com>
2021-04-10 14:55:37 +0800

    create dir

---
+++ server/request_api.c
@@ -0,0 +1,10 @@
+/**
+ * \copyright Copyright (c) 1999-2020,西安交大捷普网络科技有限公司
+ * 
+ * \file write_log.c
+ * \author xiaoke
+ * \version 0.1
+ * \date 2020-11-23
+ * 
+ * \brief 日志调试接口实现
+ */
---
+++ server/request_api.h
@@ -0,0 +1,10 @@
+/**
+ * \copyright 
+ * 
+ * \file write_log.c
+ * \author xiaoke
+ * \version 0.1
+ * \date 2020-11-23
+ * 
+ * \brief 日志调试接口实现
+ */
